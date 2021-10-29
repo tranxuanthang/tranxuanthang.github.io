edb137aec895d56b44d6b84d051fdada8bb42b78
tranxuanthang <tranthang.yb@outlook.com>
2021-10-29 11:30:28 +0700

    Add audio

--- decisive-strike/main.js
+++ decisive-strike/main.js
@@ -1,6 +1,29 @@
 import SkillCheck from "https://cdn.skypack.dev/decisive-strike@0.0.7"
 
 document.addEventListener('DOMContentLoaded', () => {
-  const skillCheck = new SkillCheck(document.getElementById('canvas'))
+  const notifyAudio = new Audio('./notify.mp3')
+  const greatAudio = new Audio('./great.mp3')
+
+  const playNotifyAudio = () => {
+      notifyAudio.pause()
+      notifyAudio.currentTime = 0.0
+      notifyAudio.play()
+  }
+
+  const playGreatAudio = () => {
+      greatAudio.pause()
+      greatAudio.currentTime = 0.0
+      greatAudio.play()
+  }
+
+  const skillCheck = new SkillCheck(document.getElementById('canvas'), {
+    isContinuously: false,
+    checkTime: 2000,
+    checkpointDifficulty: 0.08,
+    notifyBefore: 800,
+    playNotifyAudio: playNotifyAudio,
+    playGreatAudio: playGreatAudio,
+  })
+
   skillCheck.play()
 })
